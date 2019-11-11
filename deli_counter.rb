2a9859549a4ab3d65295e719191155c2b0392083
Winston Pemberton <winstonpemberton@gmail.com>
2019-11-11 23:10:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+def line(people_in_line)
+  current_line = ["The line is currently:"]
+  if people_in_line.none? == true
+    puts "The line is currently empty."
+  else
+    people_in_line.each_with_index do |people, place|
+      current_line << " #{place + 1}. #{people}"
+    end
+    puts current_line.join
+  end
+end
+
+def take_a_number(people_in_line, new_customer)
+    if people_in_line.none? == true
+      puts "The line is currently empty."
+    else
+      people_in_line.each do |place|
+         "Welcome, #{new_customer}. You are number #{place + 1} in line"
+      end
+    end
+end
